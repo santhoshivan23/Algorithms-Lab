5107e3749434810ea5cfaf8eea91ca913d8f6141
Santhoshivan <santhoshivan.a@learner.manipal.edu>
2020-01-06 15:44:52 +0530

    L0

--- Lab-0/binary_rec.cpp
+++ Lab-0/binary_rec.cpp
@@ -18,12 +18,13 @@ else cout<<key<<" found at index "<<index<<"\n";
 }
 
 
-int binarySearch(int arr,int key,int low,int high) {
+int binarySearch(int arr[],int key,int low,int high) {
 
+  if(low>high) return -1;
   int mid = (low + high)/2;
   if(arr[mid]==key) return mid;
   else if(arr[mid] < key) return binarySearch(arr,key,mid+1,high);
   else return binarySearch(arr,key,low,mid-1);
-  return -1;
+
 
 }
